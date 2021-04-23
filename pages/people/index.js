cffc0b573e9f4bbff96e848df36712a47ffa1a8b
theiconik <kumarujjawal51@gmail.com>
2021-04-23 13:25:41 +0530

    Fixed bugs and increased padding

--- pages/people/index.js
+++ pages/people/index.js
@@ -1,15 +1,14 @@
 import styles from '../../styles/people.module.css'
+import Head from "next/head"
 
 export const getStaticProps = async () => {
-  const res = await (await fetch("https://jsonplaceholder.typicode.com/users"));
+  const res = await fetch('https://jsonplaceholder.typicode.com/users/');
   const data = await res.json();
 
   return {
     props: { peoples: data }
   }
 }
-
-import Head from "next/head";
 
 const nextRoot = ({peoples}) => {
   return (
@@ -22,7 +21,7 @@ const nextRoot = ({peoples}) => {
         <h1>People</h1>
         {peoples.map(people => (
           <div key={people.id}>
-          <a className={styles.single}>{ people.name}</a>
+          <a className={styles.single}>{people.name}</a>
           </div>
         ))}
       </div>
